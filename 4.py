2b728e773d1fe070ec439f2ca249c44b9a838535
maheravi <56476428+maheravi@users.noreply.github.com>
2021-03-23 19:32:17 +0430

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,7 +1,7 @@
-sentence_string = input('Please Input Your Sentence: ')
+sentence = input('Please Input Your Sentence: ')
 
-print("The original string is : " + sentence_string)
+print("The original string is : " + sentence)
 
-WordNumber = len(sentence_string.split())
+WordNumber = len(sentence.split())
 
 print("The number of words in string are : " + str(WordNumber))
