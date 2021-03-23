f670749ca06a279b198af93ff6b59254a65e1924
maheravi <56476428+maheravi@users.noreply.github.com>
2021-03-23 19:21:18 +0430

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,7 @@
+sentence_string = input('Please Input Your Sentence: ')
+
+print("The original string is : " + sentence_string)
+
+WordNumber = len(sentence_string.split())
+
+print("The number of words in string are : " + str(WordNumber))
