ce288fa22b66c22da9937bd5c91e6c0d8a89ab23
maheravi <56476428+maheravi@users.noreply.github.com>
2021-03-23 19:35:40 +0430

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,22 @@
+list = []
+
+n = int(input("Enter number of array : "))
+
+for i in range(0, n):
+    array = int(input())
+
+    list.append(array)
+
+print(list)
+
+fn = 0
+i = 1
+while i < len(list):
+    if list[i] < list[i - 1]:
+        fn = 1
+    i += 1
+
+if not fn:
+    print("Yes, Your list is sorted.")
+else:
+    print("No, Your list is not sorted.")
