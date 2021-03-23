d81953db0904d27b72df6cf12655c5c63f12d669
maheravi <56476428+maheravi@users.noreply.github.com>
2021-03-23 19:16:58 +0430

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,35 @@
+import random
+
+score = 10
+true_chars = []
+
+words = ['Tree', 'Python', 'Android', 'True', 'Clock', 'USB']
+word = random.choice(words)
+word = word.lower()
+
+while True:
+    for i in range(len(word)):
+        if word[i] in true_chars:
+            print(word[i], end='')
+        else:
+            print('_', end='')
+    char = input('\nplease enter a character: ')
+    char = char.lower()
+
+    if true_chars == word:
+        print('You win')
+        break
+
+    if char in word:
+        true_chars.append(char)
+    else:
+        score -= 1
+
+    print(score)
+
+    if score == 0:
+        print('Game Over')
+        break
+
+
+
