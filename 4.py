497864a5ce4179613c51198747b9d488a0a4d785
maheravi <56476428+maheravi@users.noreply.github.com>
2021-03-23 19:33:28 +0430

    Delete 4.py

--- 4.py
+++
@@ -1,7 +0,0 @@
-sentence = input('Please Input Your Sentence: ')
-
-print("The sentence is : " + sentence)
-
-WordNumber = len(sentence.split())
-
-print("The number of words in sentence is : " + str(WordNumber))
